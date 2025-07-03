0557889c178e77385d24d8f26e2fb80f7f5e4996
ayabarrera <alyannabarrera@gmail.com>
2025-07-03 12:52:22 -0600

    added a user schema with username, email, password, and role

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["User", "Admin"],
+    default: "User",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
